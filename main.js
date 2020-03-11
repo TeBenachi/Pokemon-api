2523658959220e1c0e9fc64f9f650c2a4b10d885
TeBenachi <utz119@gmail.com>
2020-03-11 16:23:46 -0400

    Fixed array objects

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function setRandomInt(max) {
 }
 console.log(setRandomInt(20));
 
-fetch(`${api.base}pokemon/` + setRandomInt(20))
+fetch(`${api.base}pokemon/` + setRandomInt(150))
   .then(data => {
     return data.json();
   })
@@ -23,20 +23,27 @@ fetch(`${api.base}pokemon/` + setRandomInt(20))
 function displayResults(data) {
   console.log(data);
 
-  let name = document.querySelector(".name");
-  name.innerHTML = `${data.name}`;
+  let name = document.querySelector("#name");
+  name.innerHTML = `${data.name.toUpperCase()} `;
 
   let id = document.querySelector(".id");
   id.innerText = `ID: ${data.id}`;
 
   let species = document.querySelector(".species");
-  species.innerText = `${data.species.name}`;
+  species.innerText = `${data.species.name} pokémon species.`;
+
+  let weight = document.querySelector(".weight");
+  weight.innerText = `Weight: ${data.weight} hg`;
 
   let base_experience = document.querySelector(".experience");
   base_experience.innerText = `Required experience level: ${data.base_experience}`;
 
-  let abilities = document.querySelector(".abilities");
-  abilities.innerText = `Abilities: ${data.abilities[0].ability.name}`;
+  let displayAbilities  = abilities =
+  data.abilities.map(ability => ability.ability.name).join(", ");
+  console.log(displayAbilities);
+
+  let showAbilities = document.querySelector(".abilities");
+  showAbilities.innerHTML = `Abilities: ` + displayAbilities;  
 
   let poke_pic = document.querySelector(".poke_pic");
   poke_pic.innerHTML = `<img src="${data.sprites.front_default}" class="img-fluid" >`;
