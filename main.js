3d8151e397d5cbbf39053d9afd7d457f94953e9e
TeBenachi <utz119@gmail.com>
2020-03-12 13:17:46 -0400

    Fixed name and abilities

--- main.js
+++ main.js
@@ -23,14 +23,18 @@ fetch(`${api.base}pokemon/` + setRandomInt(150))
 function displayResults(data) {
   console.log(data);
 
+  // Make the first letter Uppercase 
   let name = document.querySelector("#name");
-  name.innerHTML = `${data.name.toUpperCase()} `;
+  name.innerHTML = `${data.name.charAt(0).toUpperCase()}${data.name.slice(1)} `;
 
   let id = document.querySelector(".id");
   id.innerText = `ID: ${data.id}`;
 
+  let species_name = document.querySelector(".species_name");
+  species_name.innerText = `${data.name.charAt(0).toUpperCase()}${data.name.slice(1)} is a species of  `;
+  
   let species = document.querySelector(".species");
-  species.innerText = `${data.species.name} pokémon species.`;
+  species.innerText = `${data.species.name.charAt(0).toUpperCase()}${data.name.slice(1)}`;
 
   let weight = document.querySelector(".weight");
   weight.innerText = `Weight: ${data.weight} hg`;
@@ -39,11 +43,14 @@ function displayResults(data) {
   base_experience.innerText = `Required experience level: ${data.base_experience}`;
 
   let displayAbilities  = abilities =
-  data.abilities.map(ability => ability.ability.name).join(", ");
+  data.abilities.map(ability => ability.ability.name.charAt(0).toUpperCase() + ability.ability.name.slice(1)).join(", ");
   console.log(displayAbilities);
 
+  let formattedAbilities = displayAbilities.replace(/-/g, ' ')
+  console.log(formattedAbilities);
+
   let showAbilities = document.querySelector(".abilities");
-  showAbilities.innerHTML = `Abilities: ` + displayAbilities;  
+  showAbilities.innerHTML = `Abilities: ` + formattedAbilities;  
 
   let poke_pic = document.querySelector(".poke_pic");
   poke_pic.innerHTML = `<img src="${data.sprites.front_default}" class="img-fluid" >`;
